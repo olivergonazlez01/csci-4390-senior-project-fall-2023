d056788eac96662e6a6b154bf23b9af2410e02e5
olivergonazlez01 <oliver.gonzalez01@utrgv.edu>
2023-10-08 21:37:36 -0500

    finished inventory

--- Senior_Project_2023/Assets/Pickup.cs
+++ Senior_Project_2023/Assets/Pickup.cs
@@ -6,30 +6,61 @@ using UnityEngine.UI;
 public class Pickup : MonoBehaviour
 {
     [SerializeField]
-    public Image[] slots;
-    public Sprite icon;
+    public Image[] icon; public GameObject[] buttons;
     public GameObject player;
+    Sprite newestItemIcon;
+    SpriteRenderer sr;
 
     private void OnTriggerStay2D(Collider2D collider) {
         if (Input.GetKey(KeyCode.E)) {
+            Transform t = player.transform;
             if (collider.gameObject.CompareTag("Player")) {
-                this.transform.parent = player.transform;
-                if (player.transform.localScale.x > 0) {
-                    this.transform.position = new Vector3(player.transform.position.x - 0.4f, player.transform.position.y, player.transform.position.z);
+                if (icon[0].gameObject.activeSelf == false) {
+                    icon[0].gameObject.SetActive(true);
+                    sr = this.gameObject.GetComponent<SpriteRenderer>();
+                    icon[0].sprite = sr.sprite;
+                    buttons[0].gameObject.GetComponent<Image>().color = Color.green;
+
+                } else if (icon[1].gameObject.activeSelf == false) {
+                    for (int i = 0; i < t.childCount; i++) {
+                        if (t.GetChild(i).gameObject.tag == "Weapon") {
+                            if (t.GetChild(i).gameObject.activeSelf) {
+                                t.GetChild(i).gameObject.SetActive(false);
+                                sr = this.gameObject.GetComponent<SpriteRenderer>();
+                                icon[1].sprite = sr.sprite;
+                                icon[1].gameObject.SetActive(true);
+                                buttons[1].GetComponent<Image>().color = Color.green;
+                                buttons[0].GetComponent<Image>().color = Color.white;
+                            } else {
+                                Destroy(t.GetChild(i).gameObject);
+                            }
+                        }
+                    }
+                } else if (icon[1].gameObject.activeSelf && icon[0].gameObject.activeSelf) {
+                    for (int i = 0; i < t.childCount; i++) {
+                        if (t.GetChild(i).gameObject.tag == "Weapon") {
+                            if (t.GetChild(i).gameObject.activeSelf) {
+                                Destroy(t.GetChild(i).gameObject);
+                            }
+                        }
+                    }
+                    for (int i = 0; i < 2; i++) {
+                        if (buttons[i].GetComponent<Image>().color == Color.green) {
+                            buttons[i].transform.GetChild(0).gameObject.GetComponent<Image>().sprite = this.gameObject.GetComponent<SpriteRenderer>().sprite;
+                        }
+                    }
+                }
+                this.transform.parent = t;
+                if (t.localScale.x > 0) {
+                    this.transform.position = new Vector3(t.position.x - 0.4f, t.position.y, t.position.z);
                 } else {
                     Vector3 flip = this.transform.localScale;
                     flip.x *= -1;
                     this.transform.localScale = flip;
-                    this.transform.position = new Vector3(player.transform.position.x + 0.4f, player.transform.position.y, player.transform.position.z);
-                }
-                for (int i = 0; i < 2; i++) {
-                    if (slots[i].sprite == null) {
-                        slots[i].sprite = icon;
-                        slots[i].enabled = true;
-                        break;
-                    }
+                    this.transform.position = new Vector3(t.position.x + 0.4f, t.position.y, t.position.z);
                 }
             }
         }
     }
 }
+
