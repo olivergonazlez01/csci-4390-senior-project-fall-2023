b2108107c5e9da3ee206c86521326be94cff9e61
adrian-nino <adrian.nino01@utrgv.edu>
2023-10-08 22:51:55 -0500

    finished farm level demo1

--- Senior_Project_2023/Assets/Hinder.cs
+++ Senior_Project_2023/Assets/Hinder.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Hinder : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Check if player is touching this tile
     void OnTriggerEnter2D(Collider2D collider) {
         Debug.Log("touched");
